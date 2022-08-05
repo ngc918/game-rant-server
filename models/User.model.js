f013184ae9cfe6285664524ab93421dfedae333b
ngc.918 <nicolas.g9182@gmail.com>
2022-08-05 09:50:53 -0400

    remove screename

--- models/User.model.js
+++ models/User.model.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
 const userSchema = new Schema({
-	screenName: { type: String, unique: true, required: true },
 	email: { type: String, unique: true, required: true },
 	pwd: { type: String, required: true },
 });
